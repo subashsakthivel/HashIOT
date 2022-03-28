b107fd996af08b8bf7215bcf1e0e37614546c9d7
Subash Sakthivel S <subashsakthivel13122001@gmail.com>
2022-03-28 12:39:33 +0530

    Update IoTash.sol

--- smart_contract/contracts/IoTash.sol
+++ smart_contract/contracts/IoTash.sol
@@ -2,34 +2,29 @@
 pragma solidity ^0.8.0;
 
 contract IoTash {
-    event GrantIOT(uint indexed index,bytes32 name, bytes32 mac, uint port , address indexed device);
+    event GrantIOT(address indexed device, bytes32 name, bytes32 mac, uint port );
     event RevokeIOT(address indexed owner ,address indexed device);
 
 
     struct IoT {
-        uint index;
         bytes32 name;
         bytes32 mac;
         uint port;
         address deviceAddress;
     }
 
-    mapping (address => uint) public INDEX;
-
     mapping (address => IoT[]) public IOTaccess;
     
     function RegisterDevice(bytes32 _name, bytes32 _mac, uint _port , address _deviceAddress) external {
-        IoT memory device = IoT(INDEX[msg.sender],_name, _mac, _port , _deviceAddress);
+        IoT memory device = IoT(_name, _mac, _port , _deviceAddress);
         IOTaccess[msg.sender].push(device);
-        INDEX[msg.sender]++;//This only Problem
-        emit GrantIOT(device.index , device.name , device.mac, device.port, device.deviceAddress);
+        emit GrantIOT(device.deviceAddress ,device.name , device.mac, device.port);
     }
     
     function RevokeDevice(uint _index) external {
         emit RevokeIOT(msg.sender, IOTaccess[msg.sender][_index].deviceAddress);
         uint len = IOTaccess[msg.sender].length;
         IOTaccess[msg.sender][_index] = IOTaccess[msg.sender][len-1];
-        INDEX[msg.sender]--;
         IOTaccess[msg.sender].pop();
     }   
 
@@ -38,7 +33,7 @@ contract IoTash {
         delete IOTaccess[msg.sender];
     }
 
-    function getIOTs() public view returns (IoT[] memory){
+    function getIOTs() external view returns (IoT[] memory){
     return IOTaccess[msg.sender];
     }
 }
